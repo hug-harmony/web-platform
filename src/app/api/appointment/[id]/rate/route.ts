491ce6e9991a8611c77621882adce77a83b0fcbe
swift code solution <dev@swiftcodesolution.com>
2025-09-26 23:49:50 +0500

    reschedule resolved again again again again again again

--- src/app/api/appointment/[id]/rate/route.ts
+++ src/app/api/appointment/[id]/rate/route.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // app/api/appointment/[id]/rate/route.ts
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import prisma from "@/lib/prisma";
 import { authOptions } from "@/lib/auth";
@@ -13,15 +13,12 @@ const schema = z.object({
   note: z.string().optional(),
 });
 
-export async function PATCH(
-  request: Request,
-  context: { params: { id: string } }
-) {
+export async function PATCH(request: NextRequest, { params }: any) {
   const session = await getServerSession(authOptions);
   if (!session?.user?.id)
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
 
-  const { id } = context.params;
+  const { id } = params;
 
   try {
     const { adjustedRate, note } = schema.parse(await request.json());
