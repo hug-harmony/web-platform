4830912f80db69953fbb9080b75dec709f38d453
swift code solution <dev@swiftcodesolution.com>
2025-11-20 01:55:48 +0500

    002 layout update + responsive

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,10 +2,9 @@
 import React from "react";
 import ClientSessionProvider from "@/components/ClientSessionProvider";
 import Sidebar from "@/components/Sidebar";
-import BottomNav from "@/components/BottomNav"; // Import the new component
+import BottomNav from "@/components/BottomNav";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import Image from "next/image";
-
 import hhIcon from "../../../public/hh-icon.png";
 import Link from "next/link";
 import { LastOnlineUpdater } from "@/components/LastOnlineUpdater";
@@ -20,7 +19,7 @@ export default function Layout({
       <SidebarProvider defaultOpen={true}>
         <div className="flex min-h-screen w-full">
           <Sidebar />
-          <main className="bg-pink-400 flex-1 transition-all duration-200 ease-in-out max-w-7xl mx-auto md:pb-0 pb-16">
+          <main className="flex-1 transition-all duration-200 ease-in-out max-w-7xl mx-auto md:pb-0 pb-16">
             <div className="bg-white p-2 w-15 h-15 absolute top-6 right-6 hidden md:flex items-center justify-center rounded-md border border-gray-200 shadow-sm z-50">
               <Link href="/dashboard">
                 <Image
